22a8fb4b64fd8fd7704187b6a7efe178b7c35c5f
Travis Lee <travisslee223@gmail.com>
2020-09-10 23:29:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ end
 
 def assign_rooms(attendees)
   assigned = []
-  attendees.each_with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
+  assigned.push(attendees.each_with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index}!"})
 end
   
