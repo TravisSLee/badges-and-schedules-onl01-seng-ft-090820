04e8658c71e3eb9625381b10a59c4865b5432ca2
Travis Lee <travisslee223@gmail.com>
2020-09-10 23:13:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |person| 
-    badge_maker(person)
+  attendees.each do |name| 
+    badge_maker(name)
   end
 end
